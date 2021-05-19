99dc7f01cb2856e5d612cc08416e255f0d842e21
Phil Stainer <phil.stainer@pm.me>
2021-05-19 10:20:43 +0100

    setup

---
+++ packages/server/src/server.ts
@@ -0,0 +1,17 @@
+import {PrismaClient} from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  // ... you will write your Prisma Client queries here
+  const allUsers = await prisma.user.findMany()
+  console.log(allUsers)
+}
+
+main()
+  .catch(e => {
+    throw e
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
