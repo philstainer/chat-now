83c5409a2db8ac91a936821dbbaa1a46176166e0
Phil Stainer <phil.stainer@pm.me>
2021-05-19 13:02:38 +0100

    feat: Add husky hooks

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': [2, {argsIgnorePattern: '^_'}],
+    'no-console': [2, {allow: ['warn', 'error']}],
+    'no-undef': 'off',
+  },
+}
--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,7 @@ const prisma = new PrismaClient()
 
 async function main() {
   // ... you will write your Prisma Client queries here
-  const allUsers = await prisma.user.findMany()
-  console.log(allUsers)
+  await prisma.user.findMany()
 }
 
 main()
