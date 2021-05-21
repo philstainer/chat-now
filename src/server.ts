24e2686067ea4201360c5dc583bfd91a8707fc7e
Phil Stainer <phil.stainer@pm.me>
2021-05-21 16:15:57 +0100

    feat: Close server on uncaughtException

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
+// Load .env when not in production
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
+// Catch uncaught exceptions
 process.on('uncaughtException', er => {
   console.error(er.stack)
+  server?.close()
   process.exit(1)
 })
 
